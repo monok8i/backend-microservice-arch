6853bde8e3525698e9e965ba715c66ac63e00348
monok8i <didkivskiyyura725@gmail.com>
2023-12-11 12:45:46 +0100

    Updated user model for database: Added referral code

--- source/backend/app/models/users.py
+++ source/backend/app/models/users.py
@@ -1,11 +1,9 @@
-from sqlalchemy import (
-    String,
-    ForeignKey,
-    Boolean,
-    Integer,
-)
-from pydantic import EmailStr
+import uuid
+
+from sqlalchemy import String, Boolean, Integer, UUID
 from sqlalchemy.orm import Mapped, mapped_column
+
+
 from .base import Base
 
 
@@ -15,3 +13,6 @@ class User(Base):
     email: Mapped[str] = mapped_column(String)
     is_active: Mapped[bool] = mapped_column(Boolean)
     is_superuser: Mapped[bool] = mapped_column(Boolean)
+    referral_code: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), default=uuid.uuid4()
+    )
