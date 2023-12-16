bc9400e40131adac3eafccb326ca923ffc6c56f5
monok8i <didkivskiyyura725@gmail.com>
2023-12-16 17:57:14 +0100

    Created startup event for creating and connection to `Redis`

--- source/backend/main.py
+++ source/backend/main.py
@@ -2,12 +2,23 @@ import uvicorn
 import uuid
 
 from fastapi import FastAPI, status
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+
+from redis.asyncio import ConnectionPool, Redis
 
 from source.backend.app.api import api_router
 from source.backend.app.core import settings
 
 app = FastAPI(title="FastAPI backend", openapi_url=f"{settings.API_V1}/openapi.json")
 app.include_router(api_router, prefix=settings.API_V1)
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    pool = ConnectionPool.from_url(url=settings.Redis.REDIS_URI)
+    redis = Redis(connection_pool=pool)
+    FastAPICache.init(RedisBackend(redis=redis), prefix='redis_cache')
 
 
 def main() -> None:
