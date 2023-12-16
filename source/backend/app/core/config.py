1b2062b1e8ab01440d461bbe2c84113c43642e8b
monok8i <didkivskiyyura725@gmail.com>
2023-12-16 17:53:24 +0100

    Added Redis for caching data

--- source/backend/app/core/config.py
+++ source/backend/app/core/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import environs
 
 from typing import Optional, Any
-from pydantic import PostgresDsn, field_validator
+from pydantic import PostgresDsn, field_validator, RedisDsn
 from pydantic_core.core_schema import FieldValidationInfo
 from pydantic_settings import BaseSettings
 
@@ -45,11 +45,37 @@ class Settings(BaseSettings):
             )
 
     @property
-    class Validation(BaseSettings):
+    class Authentication(BaseSettings):
         ACCESS_TOKEN_EXPIRE_MINUTES: int = 2
         JWT_PRIVATE_KEY: Path = env.path("JWT_PRIVATE_PATH")
         JWT_PUBLIC_KEY: Path = env.path("JWT_PUBLIC_PATH")
-        ALGORITHM: str = 'RS256'
+        ALGORITHM: str = "RS256"
+
+    @property
+    class Redis(BaseSettings):
+        REDIS_USER: Optional[str] = "default"
+        REDIS_HOST: str = env.str("REDIS_HOST")
+        REDIS_PORT: int = env.int("REDIS_PORT")
+        REDIS_PASSWORD: str = env.str("REDIS_PASSWORD")
+
+        REDIS_URI: Optional[str] = None
+
+        @field_validator(__field="REDIS_URI", mode="before")
+        @classmethod
+        def assemble_db_connection(
+            cls, v: Optional[str], info: FieldValidationInfo
+        ) -> Any:
+            if isinstance(v, str):
+                return v
+            return str(
+                RedisDsn.build(
+                    scheme="redis",
+                    username=info.data.get("REDIS_USER"),
+                    password=info.data.get("REDIS_PASSWORD"),
+                    host=info.data.get("REDIS_HOST"),
+                    port=info.data.get("REDIS_PORT"),
+                )
+            )
 
 
 settings = Settings()
