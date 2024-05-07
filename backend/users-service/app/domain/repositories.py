ebc2737c35a2aedc7613619477f651d364b15590
monok8i <didkivskiyyura725@gmail.com>
2024-05-07 21:33:55 +0200

    Added type to generic

--- backend/users-service/app/domain/repositories.py
+++ backend/users-service/app/domain/repositories.py
@@ -3,9 +3,9 @@ from advanced_alchemy.repository import SQLAlchemyAsyncRepository
 from app.database.models import User, RefreshSession
 
 
-class UserRepository(SQLAlchemyAsyncRepository):
+class UserRepository(SQLAlchemyAsyncRepository[User]):
     model_type = User
 
 
-class RefreshSessionRepository(SQLAlchemyAsyncRepository):
+class RefreshSessionRepository(SQLAlchemyAsyncRepository[RefreshSession]):
     model_type = RefreshSession
