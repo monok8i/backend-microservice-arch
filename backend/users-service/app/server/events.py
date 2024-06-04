206f3651320326c4444fd1b14d073c6cb512d50c
monok8i <didkivskiyyura725@gmail.com>
2024-06-04 20:03:08 +0200

    fix errors with closed connection (added a simple reconnect)

--- backend/users-service/app/server/events.py
+++ backend/users-service/app/server/events.py
@@ -2,13 +2,23 @@ from typing import AsyncGenerator
 from litestar import Litestar
 from contextlib import asynccontextmanager
 
+from pika.adapters import BlockingConnection
+
 
 @asynccontextmanager
 async def lifespan(app: Litestar) -> AsyncGenerator[None, None]:
     try:
         broker_callable_connection = app.state.get("rmq_session")
-        connection = broker_callable_connection()
+        connection: BlockingConnection = broker_callable_connection()
         app.state.update({"rmq_session": connection})
-        yield
+    except Exception as e:
+        reconnect: BlockingConnection = broker_callable_connection()
+        app.state.update({"rmq_session": reconnect})
+        raise e
+    
+    yield
+
+    try:
+        connection.close()
     except Exception as e:
         raise e
